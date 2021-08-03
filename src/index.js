c4db169a6392a1a09ffc9d78311dc25cdcd09b7a
Taneros <fricentaurus@GMAIL.COM>
2021-08-03 15:14:14 +0300

    fix: find solution should work now

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 module.exports = function reverse(n) {
 
-  return n.toString().split('').reverse().join('')
+
+  const arr = n.toString().split('')
+  console.log('arr', arr)
+
+  // arr.forEach((el, i) => typeof (el) === 'number' ? '' : arr.slice(i + 1));
+  const arrFiltered = arr.filter(el => !isNaN(el))
+
+  console.log('arrFiltered', arrFiltered)
+
+  return arrFiltered.reverse().join('')
+
+
 }
+
+
+/**
+ *
+ * convert to array
+ *
+ * filer using isNaN("")
+ *
+ * convert to string and return
+ *
+ *
+ */
